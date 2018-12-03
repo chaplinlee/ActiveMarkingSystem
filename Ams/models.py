d05e486bf61f5b1681f511e3364a0fcabe44f292
teriyaki <splendidzero@hotmail.com>
2018-12-03 15:01:14 +0800

    database models init

--- Ams/models.py
+++ Ams/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+# marker model
+class AdminMarker(models.Model):
+    user_name = models.CharField(max_length=200)
+    psd = models.CharField(max_length=200)
+
+
+# img set model
+class ImgSet(models.Model):
+    img_name = models.CharField(max_length=200)
+    img_cat = models.CharField(max_length=200)  # img category
+    mark_flag = models.CharField(max_length=200)
