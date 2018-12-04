1144cec1a7f940f46dd8ac841e6e3e2495e5429f
teriyaki <splendidzero@hotmail.com>
2018-12-05 00:52:36 +0800

    tiny bugs fixed

--- Ams/views.py
+++ Ams/views.py
@@ -12,39 +12,10 @@ from Ams import models
 def data_set_input(requset):
 
     unmarked = '-1'  # unmarked img flag
-    data_set_dir_path = ".\\static\\data_set\\photo_cam1\\"
-    data_set_category = "road_camera"
+    data_set_dir_path = '/data_set/photo_cam1/'
+    data_set_category = 'road_camera'
 
-    # # database connect
-    # conn = sqlite3.connect('.\db.sqlite3')
-    # c = conn.cursor()
-    #
-    # #clean test data
-    # delete_sql = """delete from Ams_imgset"""
-    # c.execute(delete_sql)
-    #
-    # path_dir = os.listdir(data_set_dir_path)
-    #
-    # for all_dir in path_dir:
-    #     # img_main_path = os.path.join(all_dir)
-    #     # img_name = data_set_dir_path+img_main_path #real img name
-    #     # img_cat = data_set_category
-    #     # mark_flag = unmarked
-    #     # print('path:'+img_main_path+' name:'+img_name)
-    #     # c.execute("insert into Ams_ImgSet (img_name,img_cat,mark_flag) values (?, ?, ?)",
-    #     #           (img_name, img_cat, mark_flag))
-    #
-    #     name = data_set_dir_path + os.path.join(all_dir)
-    #     category = data_set_category
-    #     flag = unmarked
-    #
-    #     # print('n:' + img_name + " c:" + img_cat)
-    #     c.execute("INSERT INTO Ams_imgset (img_name, img_cat, mark_flag) VALUES (?, ?, ?)",
-    #               (name, category, flag))
-    #     conn.commit()
-    # conn.close()
-
-    path_dir = os.listdir(data_set_dir_path)
+    path_dir = os.listdir('./static' + data_set_dir_path)
     for all_dir in path_dir:
 
         name = data_set_dir_path + os.path.join(all_dir)
@@ -52,10 +23,11 @@ def data_set_input(requset):
         flag = unmarked
 
         models.ImgSet.objects.create(
-            img_name = name,
-            img_cat = category,
-            mark_flag = flag
+            img_name=name,
+            img_cat=category,
+            mark_flag=flag
         )
+
 
 def login(request):
     if request.method == 'GET':
