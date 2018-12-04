f5cf524a9aab983d16c0ad33b714412f61256be8
teriyaki <splendidzero@hotmail.com>
2018-12-04 16:43:27 +0800

    12.4 Progress
    page_marking
    data set upload
    database connection

--- Ams/views.py
+++ Ams/views.py
@@ -1,11 +1,37 @@
 from django.shortcuts import render
-
+import sqlite3
+import os
+from Ams import models
 # Create your views here.
 from django.http import HttpResponse
 
-
 # def index(request):
 #     return HttpResponse("Hello, world. You're at the polls index.")
+
+def data_set_input(requset):
+
+    unmarked = -1  # unmarked img flag
+    data_set_dir_path = ".\data_set\photo_cam1\\"
+    data_set_category = "road_camera"
+
+    # database connect
+    conn = sqlite3.connect('.\db.sqlite3')
+    c = conn.cursor()
+
+    #clean test data
+    delete_sql = """delete from ImgSet"""
+    c.execute(delete_sql)
+
+    path_dir = os.listdir(data_set_dir_path)
+
+    for all_dir in path_dir:
+        img_name = #real img name
+        img_cat = data_set_category
+        img_flag = unmarked
+        c.execute("insert into ImgSet (img_name,img_cat,img_flag) values (?, ?, ?)",
+                  (img_name, img_cat, img_flag))
+        conn.commit()
+    conn.close()
 
 
 def login(request):
@@ -13,10 +39,10 @@ def login(request):
         return render(request, "login.html")
 
     else:
-        username = request.POST.get('id_username', '')
-        password = request.POST.get('id_password', '')
+        username = request.POST.get("username")
+        password = request.POST.get("password")
 
-        if username == 'admin' & password == 'admin':
+        if username == 'admin' and password == 'admin':
             return render(request, "page_marking.html")
         else:
             print('error')
@@ -24,4 +50,6 @@ def login(request):
 
 
 def marking(request):
+    # TODO
+
     return render(request, "page_marking.html")
