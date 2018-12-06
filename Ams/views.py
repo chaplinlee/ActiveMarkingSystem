78e586068fbe8a6ea8b595542aaa91a66e5710a9
teriyaki <splendidzero@hotmail.com>
2018-12-06 13:53:18 +0800

    ignore add

--- Ams/views.py
+++ Ams/views.py
@@ -9,41 +9,41 @@ from django.db.models import Q
 # def index(request):
 #     return HttpResponse("Hello, world. You're at the polls index.")
 
-# def data_set_input(requset):
-#
-#     unmarked = '-1'  # unmarked img flag
-#     data_set_dir_path = '/data_set/photo_cam1/'
-#     data_set_category = 'road_camera'
-#
-#     path_dir = os.listdir('./static' + data_set_dir_path)
-#     for all_dir in path_dir:
-#
-#         name = data_set_dir_path + os.path.join(all_dir)
-#         category = data_set_category
-#         flag = unmarked
-#
-#         models.ImgSet.objects.create(
-#             img_name=name,
-#             img_cat=category,
-#             mark_flag=flag
-#         )
+def data_set_input(requset):
+
+    unmarked = '-1'  # unmarked img flag
+    data_set_dir_path = '/data_set/photo_cam1/'
+    data_set_category = 'road_camera'
+
+    path_dir = os.listdir('./static' + data_set_dir_path)
+    for all_dir in path_dir:
+
+        name = data_set_dir_path + os.path.join(all_dir)
+        category = data_set_category
+        flag = unmarked
+
+        models.ImgSet.objects.create(
+            img_name=name,
+            img_cat=category,
+            mark_flag=flag
+        )
 
 
-def img_push(request):
-
-    img_name = '11_02_13.jpg'
-    # push = models.ImgSet.objects.get(ImgSet)
-    push = models.ImgSet.objects.filter(
-        Q(img_name__icontains=img_name)
-        & Q(mark_flag='-1')
-    )
-
-    return render(request, "page_marking.html", {'push': push})
+# def img_push(request):
+#
+#     img_name = '11_02_13.jpg'
+#     # push = models.ImgSet.objects.get(ImgSet)
+#     push = models.ImgSet.objects.filter(
+#         Q(img_name__icontains=img_name)
+#         & Q(mark_flag='-1')
+#     )
+#
+#     return render(request, "page_marking.html", {'push': push})
 
 
 def login(request):
     if request.method == 'GET':
-        # data_set_input(request)
+        data_set_input(request)
         return render(request, "login.html")
 
     else:
@@ -59,6 +59,6 @@ def login(request):
 
 def marking(request):
     # TODO:
-    if request.method == 'GET':
-        img_push(request)
+    # if request.method == 'GET':
+    #     img_push(request)
     return render(request, "page_marking.html")
